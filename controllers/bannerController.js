22a8997b268f57d31332a348d97e01cd94adea00
Shahnawaz Baig <shekhubaig17@gmail.com>
2022-09-19 17:28:45 +0530

    Update lock

--- controllers/bannerController.js
+++ controllers/bannerController.js
@@ -22,42 +22,72 @@ const upload = (bucketName) =>
         cb(null, { fieldName: file.fieldname });
       },
       key: function (req, file, cb) {
-        cb(null, `image-${Date.now()}.jpeg`);
+        const newFileName = `image-${Math.random()}${Date.now()}.jpeg`;
+        const fullPath = `CampaignMedia/` + newFileName;
+        cb(null, fullPath);
       },
     }),
   });
 
 exports.createBanner = (req, res, next) => {
   //   console.log(req.file);
-  //   console.log(req.body);
+  // console.log(req.body);
 
-  const uploadSingle = upload('bucket-influish').single('itemImage');
+  const uploadMultiple = upload('bucket-influish').fields([
+    {
+      name: 'itemImage',
+      maxCount: 3,
+    },
+    {
+      name: 'image',
+      maxCount: 3,
+    },
+  ]);
 
-  uploadSingle(req, res, async (err) => {
+  uploadMultiple(req, res, async (err) => {
     if (err) {
+      console.log('multi Error');
       console.log(err);
       return res.status(400).json({
         status: 'failure',
         message: err,
       });
     }
-    console.log(req.file.location);
-    const location = req.file.location;
 
-    const newBanner = await Banner.create({
-      title: req.body.title,
-      itemType: req.body.itemType,
-      itemImage: location,
-    });
+    console.log(req.files);
+    // const location = req.file.location;
+
+    // const newBanner = await Banner.create({
+    //   title: req.body.title,
+    //   itemType: req.body.itemType,
+    //   itemImage: location,
+    // });
 
     res.status(200).json({
       status: 'success',
-      data: newBanner,
     });
   });
 
   // console.log(newBanner);
 };
+
+// exports.singleUpload = (req, res, next) => {
+//   const uploadSingle = upload('bucket-influish').single('image');
+
+//   uploadSingle(req, res, async (err) => {
+//     if (err) {
+//       console.log('Sngle Error');
+//       console.log(err);
+//       return res.status(400).json({
+//         status: 'failure',
+//         message: err,
+//       });
+//     }
+
+//     console.log(req.file);
+//     next();
+//   });
+// };
 
 exports.getbanners = catchAsync(async (req, res, next) => {
   const banners = await Banner.find();
